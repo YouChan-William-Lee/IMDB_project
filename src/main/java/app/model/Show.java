f78d2ae749594343103e329d75ea1db8d2c57996
Jainam Doshi (s3825891) <s3825891@student.rmit.edu.au>
2021-04-27 07:13:10 +0400

    Fixing shows with new database

--- src/main/java/app/model/Show.java
+++ src/main/java/app/model/Show.java
@@ -10,23 +10,23 @@ public class Show {
     private final int showId;
     private final String showTitle;
     private final String genre;
-    private final double length;
-    private final int movie;
-    private final int series;
-    private final PCOUser PCOUser;
-    private final int year;
+    private final String length;
+    private final String movie;
+    private final String series;
+    private final String PCO;
+    private final String year;
     private final String imageAddress;
-    private final Map<String, Cast> credits_roll;
+    private final Map<String, String> credits_roll;
 
 
-    public Show(int showId, String showTitle, String genre, double length, int movie, int series, PCOUser PCOUser, int year, String imageAddress, Map<String, Cast> casts) {
+    public Show(int showId, String showTitle, String genre, String length, String movie, String series, String PCO, String year, String imageAddress, Map<String, String> casts) {
         this.showId = showId;
         this.showTitle = showTitle;
         this.genre = genre;
         this.length = length;
         this.movie = movie;
         this.series = series;
-        this.PCOUser = PCOUser;
+        this.PCO = PCO;
         this.year = year;
         this.imageAddress = imageAddress;
         this.credits_roll = casts;
@@ -38,11 +38,11 @@ public class Show {
 
     public String getGenre() { return genre; }
 
-    public String getLengthHour() { return String.valueOf((int)length); }
+    public String getLengthHour() { return length; }
 
-    public String getLengthMinute() { return String.valueOf((int)((length - (int)length)*100)); }
+    public Double getLengthMinute() { return Double.parseDouble(length) * 60; }
 
-    public int getSeries() { return series; }
+    public String getSeries() { return series; }
 
     public String getYear() { return String.valueOf(year); }
 
@@ -50,5 +50,5 @@ public class Show {
 
     public String getCover() { return this.imageAddress; }
 
-    public Map<String, Cast> getCredits_roll() { return this.credits_roll; }
+    public Map<String, String> getCredits_roll() { return this.credits_roll; }
 }
