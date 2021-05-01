3603a971696a7b0f2e57d45e48b9988db2e98a22
Jainam Doshi (s3825891) <s3825891@student.rmit.edu.au>
2021-05-01 08:08:05 +0400

    Remove pre-build data

--- src/main/java/app/dao/ShowDao.java
+++ src/main/java/app/dao/ShowDao.java
@@ -5,69 +5,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-import app.model.Show;
-import app.model.Users.User;
-
-import static app.Main.showDao;
+import app.model.ShowEntities.Show;
 
 public class ShowDao extends Database {
 
-//    private final List<Show> shows;
-
-    public ShowDao() {
-
-//        PCOUser waltDisney = new PCOUser("waltdisneypictures", "$2a$10$h.dl5J86rGH7I8bD9bZeZe", "$2a$10$h.dl5J86rGH7I8bD9bZeZeci0pDt0.VwFTGujlnEaZXPf/q7vM5wO", "Walt Disney", "Pictures", "waltdisneypictures@gmail.com", "pco");
-//        Map<String, Cast> creditsRoll1 = new HashMap<String, Cast>();
-//        Cast person1 = CastDao.getPerson("Ewan McGregor", new Date(39211200000L), "Ewan Gordon McGregor was born on March 31, 1971 in Perth, Perthshire, Scotland, to Carol Diane (Lawson) and James Charles McGregor, both teachers. His uncle is actor Denis Lawson. He was raised in Crieff. At age 16, he left Morrison Academy to join the Perth Repertory Theatre");
-//        Cast person2 = CastDao.getPerson("Natalie Portman", new Date(360878400000L), "Natalie Portman is the first person born in the 1980s to have won the Academy Award for Best Actress (for Black Swan (2010)). Natalie was born Natalie Hershlag on June 9, 1981, in Jerusalem, Israel.");
-//        creditsRoll1.put("Obi-Wan Kenoboi", person1);
-//        creditsRoll1.put("Queen Amidala", person2);
-//        Show show1 = new Show(1,"Star Wars: Episode I - The Phantom Menace", "Action",2.16,1,0, waltDisney,1999,"https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg", creditsRoll1);
-//        person1.addShow(show1);
-//        person2.addShow(show1);
-//
-//        Map<String, Cast> creditsRoll2 = new HashMap<String, Cast>();
-//        person1 = CastDao.getPerson("Ewan McGregor", new Date(39211200000L), "Ewan Gordon McGregor was born on March 31, 1971 in Perth, Perthshire, Scotland, to Carol Diane (Lawson) and James Charles McGregor, both teachers. His uncle is actor Denis Lawson. He was raised in Crieff. At age 16, he left Morrison Academy to join the Perth Repertory Theatre");
-//        person2 = CastDao.getPerson("Natalie Portman", new Date(360878400000L), "Natalie Portman is the first person born in the 1980s to have won the Academy Award for Best Actress (for Black Swan (2010)). Natalie was born Natalie Hershlag on June 9, 1981, in Jerusalem, Israel.");
-//        creditsRoll2.put("Obi-Wan Kenoboi", person1);
-//        creditsRoll2.put("Queen Amidala", person2);
-//        Show show2 = new Show(2,"Star Wars: Episode II - Attack of The Clones","Action",2.22,1,0, waltDisney,2002,"https://images-na.ssl-images-amazon.com/images/I/91WTnoEb7RL._RI_.jpg", creditsRoll2);
-//        person1.addShow(show2);
-//        person2.addShow(show2);
-//
-//        Map<String, Cast> creditsRoll3 = new HashMap<String, Cast>();
-//        person1 = CastDao.getPerson("Ewan McGregor", new Date(39211200000L), "Ewan Gordon McGregor was born on March 31, 1971 in Perth, Perthshire, Scotland, to Carol Diane (Lawson) and James Charles McGregor, both teachers. His uncle is actor Denis Lawson. He was raised in Crieff. At age 16, he left Morrison Academy to join the Perth Repertory Theatre");
-//        person2 = CastDao.getPerson("Natalie Portman", new Date(360878400000L), "Natalie Portman is the first person born in the 1980s to have won the Academy Award for Best Actress (for Black Swan (2010)). Natalie was born Natalie Hershlag on June 9, 1981, in Jerusalem, Israel.");
-//        creditsRoll3.put("Obi-Wan Kenoboi", person1);
-//        creditsRoll3.put("Queen Amidala", person2);
-//        Show show3 = new Show(3,"Star Wars: Episode III - Revenge of The Sith","Action",2.20,1,0, waltDisney,2005,"https://i.pinimg.com/originals/9d/6a/23/9d6a232c628e4aaaecfaf3dbdb1d9653.jpg", creditsRoll3);
-//        person1.addShow(show3);
-//        person2.addShow(show3);
-//
-//        Map<String, Cast> creditsRoll4 = new HashMap<String, Cast>();
-//        person1 = CastDao.getPerson("Mark Hamill", new Date(-576561600000L), "Mark Hamill is best known for his portrayal of Luke Skywalker in the original Star Wars trilogy - Star Wars: Episode IV - A New Hope (1977), Star Wars: Episode V - The Empire Strikes Back (1980), and Star Wars: Episode VI - Return of the Jedi (1983) - a role he reprised in Star Wars: Episode VII - The Force Awakens (2015), Star Wars: Episode VIII - The Last Jedi (2017) and Star Wars: Episode IX - The Rise of Skywalker (2019). He also starred and co-starred in the films Corvette Summer (1978), The Big Red One (1980), and Kingsman: The Secret Service (2014). Hamill's extensive voice acting work includes a long-standing role as the Joker, commencing with Batman: The Animated Series (1992).");
-//        person2 = CastDao.getPerson("Harrison Ford", new Date(-869544000000L), "Harrison Ford was born on July 13, 1942 in Chicago, Illinois, to Dorothy (Nidelman), a radio actress, and Christopher Ford (born John William Ford), an actor turned advertising executive. His father was of Irish and German ancestry, while his maternal grandparents were Jewish emigrants from Minsk, Belarus.");
-//        creditsRoll4.put("Luke Skywalker", person1);
-//        creditsRoll4.put("Han Solo", person2);
-//        Show show4 = new Show(4,"Star Wars: Episode V - The Empire Strikes Back","Action",2.07,1,0, waltDisney,1980,"https://fsb.zobj.net/crop.php?r=nwgfaOZ1S1Fxw-zWmyBoZFhdEjGRX4LDwAxC9uDqFc0IgeshXPexkHF083T4UsZ_SAN3e5cVTCMs-V97WTbI49ANLszc4DXedgc0NQfEcFVp7uofEGvgH1IObRxNLTqAr1i6fyIYwEHOiLOD", creditsRoll4);
-//        person1.addShow(show4);
-//        person2.addShow(show4);
-//
-//        Map<String, Cast> creditsRoll5 = new HashMap<String, Cast>();
-//        person1 = CastDao.getPerson("Mark Hamill", new Date(-576561600000L), "Mark Hamill is best known for his portrayal of Luke Skywalker in the original Star Wars trilogy - Star Wars: Episode IV - A New Hope (1977), Star Wars: Episode V - The Empire Strikes Back (1980), and Star Wars: Episode VI - Return of the Jedi (1983) - a role he reprised in Star Wars: Episode VII - The Force Awakens (2015), Star Wars: Episode VIII - The Last Jedi (2017) and Star Wars: Episode IX - The Rise of Skywalker (2019). He also starred and co-starred in the films Corvette Summer (1978), The Big Red One (1980), and Kingsman: The Secret Service (2014). Hamill's extensive voice acting work includes a long-standing role as the Joker, commencing with Batman: The Animated Series (1992).");
-//        person2 = CastDao.getPerson("Daisy Ridley", new Date(702849600000L), "Daisy Jazz Isobel Ridley is an English actress. She is best known for her breakthrough role as \"Rey\" in the 2015 film, Star Wars: Episode VII - The Force Awakens (2015). Daisy was born in Westminster, London, on April 10, 1992. She is the daughter of Louise Fawkner-Corbett and Chris Ridley.");
-//        creditsRoll5.put("Luke Skywalker", person1);
-//        creditsRoll5.put("Rey", person2);
-//        Show show5 =  new Show(5,"Star Wars: Episode IX - The Rise of Skywalker","Action",2.22,1,0, waltDisney,2019,"https://lumiere-a.akamaihd.net/v1/images/star-wars-the-rise-of-skywalker-theatrical-poster-1000_ebc74357.jpeg?region=0%2C0%2C891%2C1372", creditsRoll5);
-//        person1.addShow(show5);
-//        person2.addShow(show5);
-//
-//        shows = ImmutableList.of(show1, show2, show3, show4, show5);
-    }
-
 
     // Adding shows process
-    public static void addShow(Show show) {
+    public void addShow(Show show) {
         String sql= "insert into imdb.show(showid, show_title, genre, length, movie, series, proco_id, year, imageAddress) values(?,?,?,?,?,?,?,?,?)" ;
         try {
             PreparedStatement preparedStatement = Database.connection.prepareStatement(sql);
@@ -84,31 +28,10 @@ public class ShowDao extends Database {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-//        Show newShow = new Show(showDao.getNumberOfShows() + 1, "Titanic", "Drama", "3.14", "1", "0", "3", "1997", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp", creditsRoll);
-//        String sql= "insert into imdb.show(showid, show_title, genre, length, movie, series, proco_id, year, imageAddress) values(?,?,?,?,?,?,?,?,?)" ;
-//        try {
-//            PreparedStatement preparedStatement = Database.connection.prepareStatement(sql);
-//            preparedStatement.setInt(1, showDao.getNumberOfShows() + 1);
-//            preparedStatement.setString(2, "Titanic");
-//            preparedStatement.setString(3, "Drama");
-//            preparedStatement.setString(4, "3.14");
-//            preparedStatement.setString(5, "1");
-//            preparedStatement.setString(6, "0");
-//            preparedStatement.setString(7, "3");
-//            preparedStatement.setString(8, "1997");
-//            preparedStatement.setString(9, "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp");
-//            System.out.println("Here");
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-
-
     }
 
     // Get the list of the shows
-    public static Iterable<Show> getAllShows() {
+    public Iterable<Show> getAllShows() {
 
         List<Show> results = new ArrayList<Show>();
         String sql;
@@ -167,7 +90,7 @@ public class ShowDao extends Database {
     }
 
     // Obtaining the ID of a show
-    public static Show getShowByShowId(String showId) {
+    public Show getShowByShowId(String showId) {
         List<Show> shows = (List<Show>) getAllShows();
         for(int i = 0; i < shows.size(); i++) {
             if(shows.get(i).getShowID() == Integer.parseInt(showId)) {
@@ -208,7 +131,7 @@ public class ShowDao extends Database {
 
 
     //Adding show to the show list
-    public static void addShowToList(List<Show> list, Show s) {
+    public void addShowToList(List<Show> list, Show s) {
         list.add(s);
     }
 }
