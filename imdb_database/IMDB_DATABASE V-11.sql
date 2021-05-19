f36bc80e1632b5df27e9ea1471021307656cc045
Ci Song s3798895 <s3798895@student.rmit.edu.au>
2021-05-20 03:34:29 +0800

    add new SQL

--- imdb_database/IMDB_DATABASE V-11.sql
+++ imdb_database/IMDB_DATABASE V-11.sql
@@ -175,7 +175,6 @@ CREATE TABLE `user_review` (
   `reviewId` int NOT NULL AUTO_INCREMENT,
   `show_id` int NOT NULL,
   `user_id` varchar(45) NOT NULL,
-  `rating` int NOT NULL DEFAULT '3',
   `review` varchar(255) NOT NULL,
   `date` datetime NOT NULL,
   PRIMARY KEY (`reviewId`),
@@ -204,5 +203,47 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+--
+-- Table structure for table `user_rating`
+--
+
+DROP TABLE IF EXISTS `user_rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_rating` (
+  `ratingId` int NOT NULL AUTO_INCREMENT,
+  `show_id` int NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `rating` int NOT NULL DEFAULT '3',
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`ratingId`),
+  KEY `fk_showid_idx` (`show_id`),
+  KEY `fk_username_idx` (`user_id`),
+  CONSTRAINT `fk_showid2` FOREIGN KEY (`show_id`) REFERENCES `show` (`showid`),
+  CONSTRAINT `fk_username2` FOREIGN KEY (`user_id`) REFERENCES `account` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_feedback`
+--
+
+DROP TABLE IF EXISTS `user_feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_feedback` (
+  `feedbackId` int NOT NULL AUTO_INCREMENT,
+  `show_id` int NOT NULL,
+  `user_id` varchar(45) NOT NULL,
+  `feedback` varchar(255) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`feedbackId`),
+  KEY `fk_showid_idx` (`show_id`),
+  KEY `fk_username_idx` (`user_id`),
+  CONSTRAINT `fk_showid3` FOREIGN KEY (`show_id`) REFERENCES `show` (`showid`),
+  CONSTRAINT `fk_username3` FOREIGN KEY (`user_id`) REFERENCES `account` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 -- Dump completed on 2021-05-19 22:39:22
